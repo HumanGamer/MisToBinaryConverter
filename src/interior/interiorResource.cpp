e2cbcbc8b4dcd63c7c2d574a73ce1357ff2446e5
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-05-06 16:09:40 -0500

    Added todos

--- src/interior/interiorResource.cpp
+++ src/interior/interiorResource.cpp
@@ -1,4 +1,5 @@
 #include "interiorResource.hpp"
+#include <core/streams/fileStream.hpp>
 
 InteriorResource::InteriorResource()
 {
@@ -12,10 +13,18 @@ InteriorResource::~InteriorResource()
 
 bool InteriorResource::Load(const std::string &filename)
 {
+    FileStream file;
+    if (!file.Open(filename, FileStream::OpenMode_Read))
+        return false;
+
+    // TODO: Implement InteriorResource::Load
+
     return true;
 }
 
 bool InteriorResource::Save(const std::string &filename)
 {
+    // TODO: Implement InteriorResource::Save
+
     return true;
 }
